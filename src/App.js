505fa346a31c3104a02905dbf1c18e4554b52c64
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-03-24 23:51:37 +0300

    Prevent unnecessary re-evaluations with React.memo()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
-const App = () => (
-  <div className='app'>
-    <h1>Hi there!</h1>
-  </div>
-);
+const App = () => {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
+  return (
+    <div className='app'>
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph!</Button>
+    </div>
+  );
+};
 
 export default App;
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,14 +1,20 @@
+import React from 'react';
+
 import classes from './Button.module.css';
 
-const Button = (props) => (
-  <button
-    type={props.type || 'button'}
-    className={`${classes.button} ${props.className}`}
-    onClick={props.onClick}
-    disabled={props.disabled}
-  >
-    {props.children}
-  </button>
-);
+const Button = (props) => {
+  console.log('Button RUNNING');
 
-export default Button;
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default React.memo(Button);
